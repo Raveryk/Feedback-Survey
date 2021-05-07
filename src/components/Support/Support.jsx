dec23b3b846ef82c2cb6d6fa16f0ce51722d3700
Raveryk <raveryk@gmail.com>
2021-05-06 22:01:50 -0500

    Set up Redux in the Support Component.

--- src/components/Support/Support.jsx
+++ src/components/Support/Support.jsx
@@ -1,14 +1,18 @@
 import { useHistory } from "react-router-dom";
 import { useState } from "react";
-import { useDispatch } from 'react-redux';
+import { useDispatch } from "react-redux";
 
 function Support() {
-  const [ support, setSupport ] = useState(0);
+  const [support, setSupport] = useState(0);
 
   const history = useHistory();
 
+  const dispatch = useDispatch();
+
   const handleNext = () => {
     console.log("You clicked Next with Support");
+
+    dispatch({ type: "SET_SUPPORT", payload: support });
 
     history.push("/comments");
   };
@@ -17,9 +21,18 @@ function Support() {
     <>
       <h2>How well are you being supported?</h2>
       <h5>Scale from 1-6 where 1 = Very Poor and 6 = Very Well</h5>
-      <input type="number" max="6" min="1" required />
+      <form onChange={handleNext}>
+        <input
+          onChange={(event) => setSupport(event.target.value)}
+          value={support}
+          type="number"
+          max="6"
+          min="1"
+          required
+        />
 
-      <button onClick={handleNext}>Next</button>
+        <button type="submit">Next</button>
+      </form>
     </>
   );
 }
